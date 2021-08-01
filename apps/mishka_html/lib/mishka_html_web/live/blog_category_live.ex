3833cb9ff72484f180a546d597eb1040b0c4edfc
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2021-08-01 14:08:29 +0430

    add blog category live

--- apps/mishka_html/lib/mishka_html_web/live/blog_category_live.ex
+++ apps/mishka_html/lib/mishka_html_web/live/blog_category_live.ex
@@ -1,36 +1,61 @@
 defmodule MishkaHtmlWeb.BlogCategoryLive do
   use MishkaHtmlWeb, :live_view
 
-  alias MishkaContent.Blog.{Category, Post}
+  alias MishkaContent.Blog.{Category, Post, Like}
 
+  # TODO: done// fix category counter for client blogs live for showing to user
   @impl true
-  def mount(%{"alias_link" => _alias_link}, session, socket) do
-    if connected?(socket) do
-      Category.subscribe()
-      Post.subscribe()
+  def mount(%{"alias_link" => alias_link}, session, socket) do
+    case Category.show_by_alias_link(alias_link) do
+      {:ok, :get_record_by_field, :category, record} ->
+        if connected?(socket) do
+          subscribe()
+          Category.subscribe()
+          Post.subscribe()
+        end
+
+        Process.send_after(self(), :menu, 100)
+        user_id = Map.get(session, "user_id")
+        socket =
+          assign(socket,
+            category_id: record.id,
+            alias_link: record.alias_link,
+            page_title: "مطالب مجموعه #{record.title}",
+            page_size: 12,
+            filters: %{category_id: record.id},
+            body_color: "#40485d",
+            open_modal: false,
+            page: 1,
+            user_id: Map.get(session, "user_id"),
+            posts: Post.posts(conditions: {1, 12}, filters: %{category_id: record.id}, user_id: user_id),
+            categories: Category.categories(filters: %{})
+          )
+        {:ok, socket, temporary_assigns: [posts: [], categories: []]}
+
+      _ ->
+
+        socket =
+          socket
+          |> put_flash(:info, "چنین مجموعه ای وجود ندارد")
+          |> push_redirect(to: Routes.live_path(socket, MishkaHtmlWeb.BlogsLive))
+        {:ok, socket}
     end
 
-    Process.send_after(self(), :menu, 100)
-    # we need to input seo tags
-    socket =
-      assign(socket,
-        page_title: "بلاگ",
-        body_color: "#40485d",
-        user_id: Map.get(session, "user_id"),
-        posts: Post.posts(conditions: {1, 20}, filters: %{}, user_id: Map.get(session, "user_id")),
-        featured_posts: [],
-        categories: Category.categories(conditions: {1, 20}, filters: %{})
-      )
-      {:ok, socket, temporary_assigns: [posts: [], categories: [], featured_posts: []]}
   end
 
   @impl true
-  def handle_params(%{"page" => page}, _url, socket) do
-      # after categoey exist show category posts featured (Post.posts(conditions: {1, 5}, filters: %{priority: :featured}))
-      # chenge page_title: "بلاگ" to category exists name
-      socket =
-        socket
-        |> assign([posts: Post.posts(conditions: {page, 20}, filters: %{}, user_id: socket.assigns.user_id), page: page])
+  def handle_params(%{"page" => page, "count" => _count} = _params, _url, socket) do
+    socket =
+      socket
+      |> assign(posts: Post.posts(conditions: {page, socket.assigns.page_size}, filters: socket.assigns.filters, user_id: socket.assigns.user_id), page: page)
+      {:noreply, socket}
+  end
+
+  @impl true
+  def handle_params(%{"page" => page} = _params, _url, socket) do
+    socket =
+      socket
+      |> assign(posts: Post.posts(conditions: {page, socket.assigns.page_size}, filters: socket.assigns.filters, user_id: socket.assigns.user_id), page: page)
     {:noreply, socket}
   end
 
@@ -39,51 +64,93 @@ defmodule MishkaHtmlWeb.BlogCategoryLive do
     {:noreply, socket}
   end
 
+  @impl true
+  def handle_event("like_post", %{"post-id" => post_id}, socket) do
+    socket = with {:user_id, false} <- {:user_id, is_nil(socket.assigns.user_id)},
+         {:ok, :get_record_by_id, _error_tag, _repo_data} <- Post.show_by_id(post_id),
+         {:error, :show_by_user_and_post_id, :not_found} <- Like.show_by_user_and_post_id(socket.assigns.user_id, post_id),
+         {:ok, :add, :post_like, _like_info} <- Like.create(%{"user_id" => socket.assigns.user_id, "post_id" => post_id}) do
 
-  # it should be noted if category id and post id are same the state, then do refresh or sth
+          notify_subscribers({:liked, socket.assigns.page})
+          update_post_temporary_assigns(socket, socket.assigns.page, socket.assigns.filters, socket.assigns.user_id)
+    else
+      {:error, :get_record_by_id, _error_tag} ->
 
+        socket
+        |> put_flash(:warning, "به نظر می رسد مطلب مذکور حذف شده است.")
 
-  # @impl true
-  # def handle_info({:category, :ok, repo_record}, socket) do
-  #   case repo_record.__meta__.state do
-  #     :loaded ->
+      {:ok, :show_by_user_and_post_id, liked_record} ->
+        Like.delete(liked_record.id)
+        notify_subscribers({:liked, socket.assigns.page})
+        update_post_temporary_assigns(socket, socket.assigns.page, socket.assigns.filters, socket.assigns.user_id)
 
-  #       socket = category_assign(
-  #         socket,
-  #         params: socket.assigns.filters,
-  #         page_size: socket.assigns.page_size,
-  #         page_number: socket.assigns.page,
-  #       )
+      {:error, :show_by_user_and_post_id, :cast_error}  ->
+          socket
+          |> put_flash(:warning, "خطایی در دریافت اطلاعات وجود آماده است.")
+          |> push_redirect(to: Routes.live_path(socket, __MODULE__))
 
-  #       {:noreply, socket}
+      {:user_id, true} ->
+        socket
+        |> put_flash(:warning, "به ظاهر مشکلی وجود دارد در صورت تکرار لطفا یک بار از وب سایت خارج و دوباره وارد شوید.")
+    end
 
-  #     :deleted -> {:noreply, socket}
-  #      _ ->  {:noreply, socket}
-  #   end
-  # end
-
-  # def handle_info({:post, :ok, repo_record}, socket) do
-  #   case repo_record.__meta__.state do
-  #     :loaded ->
-
-  #       socket = post_assign(
-  #         socket,
-  #         params: socket.assigns.filters,
-  #         page_size: socket.assigns.page_size,
-  #         page_number: socket.assigns.page,
-  #       )
-
-  #       {:noreply, socket}
-
-  #     :deleted -> {:noreply, socket}
-  #      _ ->  {:noreply, socket}
-  #   end
-  # end
+    {:noreply, socket}
+  end
 
   @impl true
   def handle_info(:menu, socket) do
-    # it should be shown in Blogs menue
     ClientMenuAndNotif.notify_subscribers({:menu, "Elixir.MishkaHtmlWeb.BlogsLive"})
     {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({:category, :ok, _repo_record}, socket) do
+    {:noreply, update_category_temporary_assigns(socket)}
+  end
+
+  @impl true
+  def handle_info({:post, :ok, _repo_record}, socket) do
+    update_post_temporary_assigns(socket, socket.assigns.page, socket.assigns.filters, socket.assigns.user_id)
+  end
+
+  @impl true
+  def handle_info({:liked, page}, socket) do
+    socket = if page == socket.assigns.page do
+      update_post_temporary_assigns(socket, socket.assigns.page, socket.assigns.filters, socket.assigns.user_id)
+    else
+      socket
+    end
+    {:noreply, socket}
+  end
+
+
+  defp priority(priority) do
+    case priority do
+      :none -> "ندارد"
+      :low -> "پایین"
+      :medium -> "متوسط"
+      :high -> "بالا"
+      :featured -> "ویژه"
+    end
+  end
+
+  defp update_post_temporary_assigns(socket, page, _filters, user_id) do
+    update(socket, :posts, fn _posts ->
+      Post.posts(conditions: {page, socket.assigns.page_size}, filters: socket.assigns.filters, user_id: user_id)
+    end)
+  end
+
+  defp update_category_temporary_assigns(socket) do
+    update(socket, :categories, fn _categories ->
+      Category.categories(filters: %{})
+    end)
+  end
+
+  def subscribe do
+    Phoenix.PubSub.subscribe(MishkaHtml.PubSub, "client_blogs")
+  end
+
+  def notify_subscribers({:liked, user_page}) do
+    Phoenix.PubSub.broadcast(MishkaHtml.PubSub, "client_blogs", {:liked, user_page})
   end
 end

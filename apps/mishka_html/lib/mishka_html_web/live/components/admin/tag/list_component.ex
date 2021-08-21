55a14320edd030d5f45288d8412c0d45d3b75297
shahryarjb <shahryar.tbiz@gmail.com>
2021-08-21 15:57:33 +0430

    fix subscription admin mobile view and its schema error handler, and improve tags of admin for mobile

--- apps/mishka_html/lib/mishka_html_web/live/components/admin/tag/list_component.ex
+++ apps/mishka_html/lib/mishka_html_web/live/components/admin/tag/list_component.ex
@@ -4,93 +4,67 @@ defmodule MishkaHtmlWeb.Admin.Tag.ListComponent do
   def render(assigns) do
     ~L"""
       <div class="col bw admin-blog-post-list">
-        <div class="row vazir">
-            <div class="row vazir">
-                <div class="col titile-of-blog-posts alert alert-primary">
-                    تیتر
-                </div>
-
-                <div class="col titile-of-blog-posts alert alert-danger">
-                    تیتر سفارشی
-                </div>
-
-                <div class="col titile-of-blog-posts alert alert-success">
-                    رباط
-                </div>
-
-                <div class="col titile-of-blog-posts alert alert-info">
-                    ثبت
-                </div>
-
-                <div class="col titile-of-blog-posts alert alert-danger">
-                    به روز رسانی
-                </div>
-
-                <div class="col titile-of-blog-posts alert alert-warning">
-                    عملیات
-                </div>
-            </div>
-
-            <div class="clearfix"></div>
-            <div class="space40"></div>
-            <div class="clearfix"></div>
-
-            <%= for {item, color} <- Enum.zip(@tags, Stream.cycle(["wlist", "glist"])) do %>
-                <div class="row blog-list vazir <%= if color == "glist", do: "odd-list-of-blog-posts" %>">
-                    <div class="col">
-                        <%= item.title %>
-                    </div>
-
-                    <div class="col">
-                    <span class="badge bg-primary vazir">
-                        <%= if(is_nil(item.custom_title), do: "ندارد", else: item.custom_title) %>
-                    </span>
-                    </div>
-
-                    <div class="col">
-                        <%= item.robots %>
-                    </div>
-
-                    <div class="col">
-                        <%= live_component @socket, MishkaHtmlWeb.Public.TimeConverterComponent,
+        <div class="table-responsive">
+            <table class="table vazir">
+                <thead>
+                    <tr>
+                        <th scope="col" id="div-image">تیتر</th>
+                        <th scope="col" id="div-title">تیتر سفارشی</th>
+                        <th scope="col" id="div-category">رباط</th>
+                        <th scope="col" id="div-status">ثبت</th>
+                        <th scope="col" id="div-priority">به روز رسانی</th>
+                        <th scope="col" id="div-opration">عملیات</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <%= for {item, color} <- Enum.zip(@tags, Stream.cycle(["wlist", "glist"])) do %>
+                    <tr class="blog-list vazir <%= if color == "glist", do: "odd-list-of-blog-posts" %>">
+                        <td class="align-middle text-center">
+                            <%= item.title %>
+                        </td>
+                        <td class="align-middle text-center">
+                            <%= if(is_nil(item.custom_title), do: "ندارد", else: item.custom_title) %>
+                        </td>
+                        <td class="align-middle text-center" id="<%= "title-#{item.id}" %>">
+                            <%= item.robots %>
+                        </td>
+                        <td class="align-middle text-center">
+                            <%= live_component @socket, MishkaHtmlWeb.Public.TimeConverterComponent,
                             span_id: "inserted-#{item.id}-component",
                             time: item.inserted_at
-                        %>
-                    </div>
-
-                    <div class="col">
-                        <%= live_component @socket, MishkaHtmlWeb.Public.TimeConverterComponent,
+                            %>
+                        </td>
+                        <td class="align-middle text-center">
+                            <%= live_component @socket, MishkaHtmlWeb.Public.TimeConverterComponent,
                             span_id: "updated-#{item.id}-component",
                             time: item.updated_at
-                        %>
-                    </div>
+                            %>
+                        </td>
+                        <td  class="align-middle text-center" id="<%= "opration-#{item.id}" %>">
+                            <%= live_redirect "ویرایش",
+                            to: Routes.live_path(@socket, MishkaHtmlWeb.AdminBlogTagLive, id: item.id),
+                            class: "btn btn-outline-info vazir"
+                            %>
 
-                    <div class="col opration-post-blog">
-                        <%= live_redirect "ویرایش",
-                        to: Routes.live_path(@socket, MishkaHtmlWeb.AdminBlogTagLive, id: item.id),
-                        class: "btn btn-outline-info vazir"
-                        %>
-
-                    <a class="btn btn-outline-danger vazir",
-                                phx-click="delete"
-                                phx-value-id="<%= item.id %>">حذف</a>
-                    </div>
-                </div>
-                <div class="space20"></div>
-                <div class="clearfix"></div>
+                            <a class="btn btn-outline-danger vazir" phx-click="delete" phx-value-id="<%= item.id %>">حذف</a>
+                        </td>
+                    </tr>
+                    <% end %>
+                </tbody>
+            </table>
+            <div class="space20"></div>
+            <div class="col-sm-10">
+                <%= if @tags.entries != [] do %>
+                <%= live_component @socket, MishkaHtmlWeb.Public.PaginationComponent ,
+                                id: :pagination,
+                                pagination_url: @pagination_url,
+                                data: @tags,
+                                filters: @filters,
+                                count: @count
+                %>
             <% end %>
+            </div>
         </div>
-
-        <div class="space20"></div>
-        <%= if @tags.entries != [] do %>
-            <%= live_component @socket, MishkaHtmlWeb.Public.PaginationComponent ,
-                            id: :pagination,
-                            pagination_url: @pagination_url,
-                            data: @tags,
-                            filters: @filters,
-                            count: @count
-            %>
-        <% end %>
       </div>
     """
   end

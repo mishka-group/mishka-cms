a06a14df4b087a5d61af75e5bd4ea6d940e4aff7
shahryarjb <shahryar.tbiz@gmail.com>
2021-08-26 01:43:38 +0430

    edite prod config to implement new os variable

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,10 +4,17 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
+secret_key_base_html =
+  System.get_env("SECRET_KEY_BASE_HTML") ||
     raise """
-    environment variable SECRET_KEY_BASE is missing.
+    environment variable SECRET_KEY_BASE_HTML is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+secret_key_base_api =
+  System.get_env("SECRET_KEY_BASE_API") ||
+    raise """
+    environment variable SECRET_KEY_BASE_API is missing.
     You can generate one by calling: mix phx.gen.secret
     """
 
@@ -16,7 +23,7 @@ config :mishka_html, MishkaHtmlWeb.Endpoint,
       port: String.to_integer(System.get_env("PORT") || "4000"),
       transport_options: [socket_opts: [:inet6]]
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base_html
 
 
 config :mishka_api, MishkaApiWeb.Endpoint,
@@ -24,7 +31,7 @@ config :mishka_api, MishkaApiWeb.Endpoint,
       port: String.to_integer(System.get_env("PORT") || "4001"),
       transport_options: [socket_opts: [:inet6]]
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base_api
 
 # ## Using releases (Elixir v1.9+)
 #

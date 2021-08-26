924c4668658d096e7c6023595161029b29e1ef6d
shahryarjb <shahryar.tbiz@gmail.com>
2021-08-26 15:07:33 +0430

    add @spec for mishka_content

--- apps/mishka_content/lib/blog/author.ex
+++ apps/mishka_content/lib/blog/author.ex
@@ -8,25 +8,48 @@ defmodule MishkaContent.Blog.Author do
           error_atom: :blog_author,
           repo: MishkaDatabase.Repo
 
+
+  @type data_uuid() :: Ecto.UUID.t
+  @type record_input() :: map()
+  @type error_tag() :: :blog_author
+  @type repo_data() :: Ecto.Schema.t()
+  @type repo_error() :: Ecto.Changeset.t()
+
   @behaviour MishkaDatabase.CRUD
 
-
+  @spec create(record_input()) ::
+  {:error, :add, error_tag(), repo_error()} | {:ok, :add, error_tag(), repo_data()}
   def create(attrs) do
     crud_add(attrs)
   end
 
+  @spec create(record_input(), allowed_fields :: list()) ::
+  {:error, :add, error_tag(), repo_error()} | {:ok, :add, error_tag(), repo_data()}
   def create(attrs, allowed_fields) do
     crud_add(attrs, allowed_fields)
   end
 
+  @spec edit(record_input()) ::
+  {:error, :edit, :uuid, error_tag()} |
+  {:error, :edit, :get_record_by_id, error_tag()} |
+  {:error, :edit, error_tag(), repo_error()} | {:ok, :edit, error_tag(), repo_data()}
   def edit(attrs) do
     crud_edit(attrs)
   end
 
+  @spec delete(data_uuid()) ::
+  {:error, :delete, :uuid, error_tag()} |
+  {:error, :delete, :get_record_by_id, error_tag()} |
+  {:error, :delete, :forced_to_delete, error_tag()} |
+  {:error, :delete, error_tag(), repo_error()} | {:ok, :delete, error_tag(), repo_data()}
   def delete(id) do
     crud_delete(id)
   end
 
+  @spec delete(data_uuid(), data_uuid()) ::
+          {:error, :delete, :blog_author | :forced_to_delete | :get_record_by_id | :uuid,
+           :blog_author | :not_found | Ecto.Changeset.t()}
+          | {:ok, :delete, :blog_author, %{optional(atom) => any}}
   def delete(user_id, post_id) do
     from(author in BlogAuthor, where: author.user_id == ^user_id and author.post_id == ^post_id)
     |> MishkaDatabase.Repo.one()
@@ -38,10 +61,13 @@ defmodule MishkaContent.Blog.Author do
     Ecto.Query.CastError -> {:error, :delete, :blog_author, :not_found}
   end
 
+  @spec show_by_id(data_uuid()) ::
+          {:error, :get_record_by_id, error_tag()} | {:ok, :get_record_by_id, error_tag(), repo_data()}
   def show_by_id(id) do
     crud_get_record(id)
   end
 
+  @spec authors(data_uuid()) :: list()
   def authors(post_id) do
     from(author in BlogAuthor,
     where: author.post_id == ^post_id,
@@ -59,6 +85,7 @@ defmodule MishkaContent.Blog.Author do
     Ecto.Query.CastError -> []
   end
 
+  @spec authors :: Ecto.Query.t()
   def authors() do
     from(author in BlogAuthor,
     join: user in assoc(author, :users),
@@ -70,6 +97,7 @@ defmodule MishkaContent.Blog.Author do
     })
   end
 
+  @spec allowed_fields(:atom | :string) :: nil | list
   def allowed_fields(:atom), do: BlogAuthor.__schema__(:fields)
   def allowed_fields(:string), do: BlogAuthor.__schema__(:fields) |> Enum.map(&Atom.to_string/1)
 end
--- apps/mishka_content/lib/general/subscription.ex
+++ apps/mishka_content/lib/general/subscription.ex
@@ -7,27 +7,48 @@ defmodule MishkaContent.General.Subscription do
           error_atom: :subscription,
           repo: MishkaDatabase.Repo
 
+  @type data_uuid() :: Ecto.UUID.t
+  @type record_input() :: map()
+  @type error_tag() :: :subscription
+  @type repo_data() :: Ecto.Schema.t()
+  @type repo_error() :: Ecto.Changeset.t()
+
   @behaviour MishkaDatabase.CRUD
 
   def subscribe do
     Phoenix.PubSub.subscribe(MishkaHtml.PubSub, "subscription")
   end
 
+  @spec create(record_input()) ::
+  {:error, :add, error_tag(), repo_error()} | {:ok, :add, error_tag(), repo_data()}
   def create(attrs) do
     crud_add(attrs)
     |> notify_subscribers(:subscription)
   end
 
+  @spec edit(record_input()) ::
+  {:error, :edit, :uuid, error_tag()} |
+  {:error, :edit, :get_record_by_id, error_tag()} |
+  {:error, :edit, error_tag(), repo_error()} | {:ok, :edit, error_tag(), repo_data()}
   def edit(attrs) do
     crud_edit(attrs)
     |> notify_subscribers(:subscription)
   end
 
+  @spec delete(data_uuid()) ::
+  {:error, :delete, :uuid, error_tag()} |
+  {:error, :delete, :get_record_by_id, error_tag()} |
+  {:error, :delete, :forced_to_delete, error_tag()} |
+  {:error, :delete, error_tag(), repo_error()} | {:ok, :delete, error_tag(), repo_data()}
   def delete(id) do
     crud_delete(id)
     |> notify_subscribers(:subscription)
   end
 
+  @spec delete(data_uuid(), data_uuid()) ::
+          {:error, :delete, :forced_to_delete | :get_record_by_id | :subscription | :uuid,
+           :not_found | :subscription | Ecto.Changeset.t()}
+          | {:ok, :delete, :subscription, %{optional(atom) => any}}
   def delete(user_id, section_id) do
     from(sub in Subscription, where: sub.user_id == ^user_id and sub.section_id == ^section_id)
     |> MishkaDatabase.Repo.one()
@@ -39,11 +60,14 @@ defmodule MishkaContent.General.Subscription do
     Ecto.Query.CastError -> {:error, :delete, :subscription, :not_found}
   end
 
+  @spec show_by_id(data_uuid()) ::
+          {:error, :get_record_by_id, error_tag()} | {:ok, :get_record_by_id, error_tag(), repo_data()}
   def show_by_id(id) do
     crud_get_record(id)
   end
 
-  # it should be asked how can we create  multi params of advanced search
+
+  @spec subscriptions([{:conditions, {integer() | String.t(), integer() | String.t()}} | {:filters, map()}, ...]) :: Scrivener.Page.t()
   def subscriptions(conditions: {page, page_size}, filters: filters) do
     from(sub in Subscription, join: user in assoc(sub, :users)) |> convert_filters_to_where(filters)
     |> fields()
@@ -83,9 +107,11 @@ defmodule MishkaContent.General.Subscription do
     }
   end
 
+  @spec allowed_fields(:atom | :string) :: nil | list
   def allowed_fields(:atom), do: Subscription.__schema__(:fields)
   def allowed_fields(:string), do: Subscription.__schema__(:fields) |> Enum.map(&Atom.to_string/1)
 
+  @spec notify_subscribers(tuple(), atom() | String.t()) :: tuple() | map()
   def notify_subscribers({:ok, _, :subscription, repo_data} = params, type_send) do
     Phoenix.PubSub.broadcast(MishkaHtml.PubSub, "subscription", {type_send, :ok, repo_data})
     params

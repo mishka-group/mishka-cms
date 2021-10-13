886396fbfe27f3c56792698c85971d6b7c4116c7
shahryarjb <shahryar.tbiz@gmail.com>
2021-10-13 23:46:25 +0330

    add admin list of notifs, improve ui of notif client

--- apps/mishka_content/lib/general/notif.ex
+++ apps/mishka_content/lib/general/notif.ex
@@ -15,29 +15,38 @@ defmodule MishkaContent.General.Notif do
 
   @behaviour MishkaDatabase.CRUD
 
+  def subscribe do
+    Phoenix.PubSub.subscribe(MishkaHtml.PubSub, "notif")
+  end
+
   @doc delegate_to: {MishkaDatabase.CRUD, :crud_add, 1}
   def create(attrs) do
     crud_add(attrs)
+    |> notify_subscribers(:notif)
   end
 
   @doc delegate_to: {MishkaDatabase.CRUD, :crud_add, 1}
   def create(attrs, allowed_fields) do
     crud_add(attrs, allowed_fields)
+    |> notify_subscribers(:notif)
   end
 
   @doc delegate_to: {MishkaDatabase.CRUD, :crud_edit, 1}
   def edit(attrs) do
     crud_edit(attrs)
+    |> notify_subscribers(:notif)
   end
 
   @doc delegate_to: {MishkaDatabase.CRUD, :crud_edit, 1}
   def edit(attrs, allowed_fields) do
     crud_edit(attrs, allowed_fields)
+    |> notify_subscribers(:notif)
   end
 
   @doc delegate_to: {MishkaDatabase.CRUD, :crud_delete, 1}
   def delete(id) do
     crud_delete(id)
+    |> notify_subscribers(:notif)
   end
 
   @doc delegate_to: {MishkaDatabase.CRUD, :crud_get_record, 1}
@@ -63,13 +72,18 @@ defmodule MishkaContent.General.Notif do
     |> MishkaDatabase.Repo.paginate(page: page, page_size: page_size)
   rescue
     db_error ->
+      IO.inspect(db_error)
       MishkaContent.db_content_activity_error("notif", "read", db_error)
       %Scrivener.Page{entries: [], page_number: 1, page_size: page_size, total_entries: 0,total_pages: 1}
   end
 
   defp convert_filters_to_where(query, filters) do
     Enum.reduce(filters, query, fn {key, value}, query ->
-      from notif in query, where: field(notif, ^key) == ^value
+      cond do
+        is_list(value) ->
+          from notif in query, where: field(notif, ^key) in ^value
+        true -> from notif in query, where: field(notif, ^key) == ^value
+      end
     end)
   end
 
@@ -91,7 +105,9 @@ defmodule MishkaContent.General.Notif do
       user_id: notif.user_id,
       type: notif.type,
       target: notif.target,
-      status_type: status.type
+      status_type: status.type,
+      inserted_at: notif.inserted_at,
+      updated_at: notif.updated_at
     }
   end
 
@@ -110,14 +126,11 @@ defmodule MishkaContent.General.Notif do
       extra: notif.extra,
       user_id: notif.user_id,
       type: notif.type,
-      target: notif.target
+      target: notif.target,
+      inserted_at: notif.inserted_at,
+      updated_at: notif.updated_at
     }
   end
-
-  @spec allowed_fields(:atom | :string) :: nil | list
-  def allowed_fields(:atom), do: Notif.__schema__(:fields)
-  def allowed_fields(:string), do: Notif.__schema__(:fields) |> Enum.map(&Atom.to_string/1)
-
 
   # TODO: Create a link creator for navigating to page concerned
   # TODO: top list should create a link when user clicks on a notification
@@ -137,4 +150,15 @@ defmodule MishkaContent.General.Notif do
       MishkaContent.db_content_activity_error("notif", "read", db_error)
       0
   end
+
+  @spec allowed_fields(:atom | :string) :: nil | list
+  def allowed_fields(:atom), do: Notif.__schema__(:fields)
+  def allowed_fields(:string), do: Notif.__schema__(:fields) |> Enum.map(&Atom.to_string/1)
+
+  def notify_subscribers({:ok, _, :notif, repo_data} = params, type_send) do
+    Phoenix.PubSub.broadcast(MishkaHtml.PubSub, "notif", {type_send, :ok, repo_data})
+    params
+  end
+
+  def notify_subscribers(params, _), do: params
 end

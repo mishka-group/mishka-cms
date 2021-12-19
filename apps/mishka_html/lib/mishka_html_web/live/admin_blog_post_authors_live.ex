53edc8b496d88fc0be31f857e151d6ad08bfc932
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2021-12-19 23:54:58 +0330

    improve blog admin Authors

--- apps/mishka_html/lib/mishka_html_web/live/admin_blog_post_authors_live.ex
+++ apps/mishka_html/lib/mishka_html_web/live/admin_blog_post_authors_live.ex
@@ -2,6 +2,8 @@ defmodule MishkaHtmlWeb.AdminBlogPostAuthorsLive do
   use MishkaHtmlWeb, :live_view
 
   alias MishkaContent.Blog.Author
+  @section_title MishkaTranslator.Gettext.dgettext("html_live", "مدیریت نویسندگان")
+
   use MishkaHtml.Helpers.LiveCRUD,
     module: MishkaContent.Blog.Author,
     redirect: __MODULE__,
@@ -9,7 +11,21 @@ defmodule MishkaHtmlWeb.AdminBlogPostAuthorsLive do
 
   @impl true
   def render(assigns) do
-    Phoenix.View.render(MishkaHtmlWeb.AdminBlogView, "admin_blog_post_authors_live.html", assigns)
+    ~H"""
+      <.live_component
+        module={MishkaHtml.Helpers.ListContainerComponent}
+        id={:list_container}
+        flash={@flash}
+        section_info={section_info(assigns, @socket)}
+        filters={@filters}
+        list={@authors}
+        url={MishkaHtmlWeb.AdminBlogPostAuthorsLive}
+        page_size={@page_size}
+        parent_assigns={assigns}
+        admin_menu={live_render(@socket, AdminMenu, id: :admin_menu)}
+        left_header_side=""
+      />
+    """
   end
 
   @impl true
@@ -18,7 +34,9 @@ defmodule MishkaHtmlWeb.AdminBlogPostAuthorsLive do
       {:ok, :get_record_by_id, _error_tag, _record} ->
         Process.send_after(self(), :menu, 100)
         assign(socket,
-          page_title: MishkaTranslator.Gettext.dgettext("html_live", "مدیریت نویسندگان"),
+          page_size: 20,
+          filters: %{},
+          page_title: @section_title,
           body_color: "#a29ac3cf",
           user_id: Map.get(session, "user_id"),
           authors: Author.authors(post_id),
@@ -113,5 +131,100 @@ defmodule MishkaHtmlWeb.AdminBlogPostAuthorsLive do
   @impl true
   def handle_info(_, socket) do
     {:noreply, socket}
+  end
+
+  defp author_temporary_image() do
+    """
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-cup" viewBox="0 0 16 16">
+      <path d="M1 2a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v1h.5A1.5 1.5 0 0 1 16 4.5v7a1.5 1.5 0 0 1-1.5 1.5h-.55a2.5 2.5 0 0 1-2.45 2h-8A2.5 2.5 0 0 1 1 12.5V2zm13 10h.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5H14v8zM13 2H2v10.5A1.5 1.5 0 0 0 3.5 14h8a1.5 1.5 0 0 0 1.5-1.5V2z"></path>
+    </svg>
+    """
+  end
+
+  def section_fields() do
+    [
+      ListItemComponent.custom_field("author_image", [1], "col header1", MishkaTranslator.Gettext.dgettext("html_live",  "تصویر"), author_temporary_image(),
+      {true, false, false}),
+      ListItemComponent.link_field("user_full_name", [1], "col header2", MishkaTranslator.Gettext.dgettext("html_live",  "نویسنده"),
+      {MishkaHtmlWeb.AdminUserLive, :user_id},
+      {true, false, false}),
+      ListItemComponent.time_field("inserted_at", [1], "col header3", MishkaTranslator.Gettext.dgettext("html_live",  "ثبت"), false,
+      {true, false, false})
+    ]
+  end
+
+  def section_info(assigns, socket) do
+    %{
+      section_btns: %{
+        header: [
+          %{
+            title: MishkaTranslator.Gettext.dgettext("html_live_templates", "برگشت به مطالب"),
+            router: Routes.live_path(socket, MishkaHtmlWeb.AdminBlogPostsLive),
+            class: "btn btn-outline-danger"
+          }
+        ],
+        list_item: [
+          %{
+            method: :delete,
+            router: nil,
+            title: MishkaTranslator.Gettext.dgettext("html_live",  "حذف"),
+            class: "btn btn-outline-danger vazir"
+          }
+        ]
+      },
+      title: @section_title,
+      activities_info: %{
+        title: MishkaTranslator.Gettext.dgettext("html_live_component", "نویسندگان"),
+        section_type: MishkaTranslator.Gettext.dgettext("html_live_component", "نویسنده"),
+        action: :section,
+        action_by: :section,
+      },
+      custom_operations: nil,
+      description:
+      ~H"""
+        <%= MishkaTranslator.Gettext.dgettext("html_live_templates", "در این بخش می توانید به تعداد نویسندگان یک مطلب اضافه یا کم کنید.") %>
+        <div class="space30"></div>
+        <div class="clearfix"></div>
+        <form  phx-change="search_user" id="UserFormSearch">
+          <div class="row vazir">
+              <div class="col-sm-3" id="UserFullNameSearch">
+                  <label for="country" class="form-label"><%= MishkaTranslator.Gettext.dgettext("html_live_templates", "نام کامل") %></label>
+                  <div class="space10"> </div>
+                  <input type="text" class="title-input-text form-control" id="full_name" name="full_name">
+                  <div class="col space10"> </div>
+              </div>
+              <div class="col-sm-3" id="RoleID">
+              <label for="role" class="form-label"><%= MishkaTranslator.Gettext.dgettext("html_live_templates", "نقش") %></label>
+              <div class="col space10"> </div>
+              <select class="form-select" id="role-search" name="role">
+                <option value="" selected><%= MishkaTranslator.Gettext.dgettext("html_live_templates", "انتخاب") %></option>
+                <%= for role <- MishkaUser.Acl.Role.roles() do %>
+                  <option value={role.id}><%= role.display_name %></option>
+                <% end %>
+              </select>
+            </div>
+          </div>
+        </form>
+        <div class="clearfix"></div>
+        <div class="col space30"> </div>
+
+        <div class="col">
+        <%= for {user, color} <- Enum.zip(@search_author, Stream.cycle(["warning", "info", "danger", "success", "primary"])) do %>
+            <a class={"col-sm-6 vazir list-group-item list-group-item-#{color}"} aria-current="true" id={user.id} phx-click="add_author" phx-value-user-id={user.id}>
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1"><%= MishkaHtml.full_name_sanitize(user.full_name) %></h4>
+                    <small>
+                    <.live_component module={MishkaHtmlWeb.Public.TimeConverterComponent}
+                            id={"inserted-#{user.id}-component"}
+                            span_id={"inserted-#{user.id}-component"}
+                            time={user.inserted_at}
+                    />
+                    </small>
+                </div>
+            </a>
+        <% end %>
+        </div>
+      """
+    }
   end
 end

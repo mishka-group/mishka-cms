9632a81554aee2825f107308a7f95cee9892864c
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-01-01 21:53:14 +0330

    delete CRUD macro and using mishka_developer_tools v0.0.2

--- apps/mishka_content/lib/blog/tag.ex
+++ apps/mishka_content/lib/blog/tag.ex
@@ -3,7 +3,7 @@ defmodule MishkaContent.Blog.Tag do
   alias MishkaDatabase.Schema.MishkaContent.Blog.Post
 
   import Ecto.Query
-  use MishkaDatabase.CRUD,
+  use MishkaDeveloperTools.DB.CRUD,
           module: BlogTag,
           error_atom: :blog_tag,
           repo: MishkaDatabase.Repo
@@ -14,43 +14,43 @@ defmodule MishkaContent.Blog.Tag do
   @type repo_data() :: Ecto.Schema.t()
   @type repo_error() :: Ecto.Changeset.t()
 
-  @behaviour MishkaDatabase.CRUD
+  @behaviour MishkaDeveloperTools.DB.CRUD
 
   def subscribe do
     Phoenix.PubSub.subscribe(MishkaHtml.PubSub, "blog_tag")
   end
 
-  @doc delegate_to: {MishkaDatabase.CRUD, :crud_add, 1}
+  @doc delegate_to: {MishkaDeveloperTools.DB.CRUD, :crud_add, 1}
   def create(attrs) do
     crud_add(attrs)
     |> notify_subscribers(:tag)
   end
 
-  @doc delegate_to: {MishkaDatabase.CRUD, :crud_add, 1}
+  @doc delegate_to: {MishkaDeveloperTools.DB.CRUD, :crud_add, 1}
   def create(attrs, allowed_fields) do
     crud_add(attrs, allowed_fields)
     |> notify_subscribers(:tag)
   end
 
-  @doc delegate_to: {MishkaDatabase.CRUD, :crud_edit, 1}
+  @doc delegate_to: {MishkaDeveloperTools.DB.CRUD, :crud_edit, 1}
   def edit(attrs) do
     crud_edit(attrs)
     |> notify_subscribers(:tag)
   end
 
-  @doc delegate_to: {MishkaDatabase.CRUD, :crud_edit, 1}
+  @doc delegate_to: {MishkaDeveloperTools.DB.CRUD, :crud_edit, 1}
   def edit(attrs, allowed_fields) do
     crud_edit(attrs, allowed_fields)
     |> notify_subscribers(:tag)
   end
 
-  @doc delegate_to: {MishkaDatabase.CRUD, :crud_delete, 1}
+  @doc delegate_to: {MishkaDeveloperTools.DB.CRUD, :crud_delete, 1}
   def delete(id) do
     crud_delete(id)
     |> notify_subscribers(:tag)
   end
 
-  @doc delegate_to: {MishkaDatabase.CRUD, :crud_get_record, 1}
+  @doc delegate_to: {MishkaDeveloperTools.DB.CRUD, :crud_get_record, 1}
   def show_by_id(id) do
     crud_get_record(id)
   end
@@ -59,6 +59,7 @@ defmodule MishkaContent.Blog.Tag do
   def tags(conditions: {page, page_size}, filters: filters) do
     query = from(tag in BlogTag) |> convert_filters_to_where_tag(filters)
     from([tag] in query,
+    order_by: [desc: tag.inserted_at, desc: tag.id],
     select: %{
       id: tag.id,
       title: tag.title,

c4fbf0f0ef2d14a3aa6880f4e853680c74fb56f8
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-01-09 13:30:45 +0330

    fix PluginState registery, add save function, Preparing PluginStateDynamicSupervisor

--- apps/mishka_installer/lib/plugin_manager/state/plugin_state_dynamic_supervisor.ex
+++ apps/mishka_installer/lib/plugin_manager/state/plugin_state_dynamic_supervisor.ex
@@ -1,22 +1,31 @@
 defmodule MishkaInstaller.PluginStateDynamicSupervisor do
 
+
   @spec start_job(map()) :: :ignore | {:error, any} | {:ok, pid} | {:ok, pid, any}
   def start_job(args) do
     DynamicSupervisor.start_child(MishkaInstaller.Cache.PluginStateOtpRunner, {MishkaInstaller.PluginState, args})
   end
 
-  @spec running_imports :: [any]
-  def running_imports() do
-    match_all = {:"$1", :"$2", :"$3"}
-    guards = [{:"==", :"$3", "plugin_state"}]
-    map_result = [%{id: :"$1", pid: :"$2", type: :"$3"}]
+
+  def running_imports(), do: registery()
+
+  def running_imports(event_name) do
+    [{:"==", :"$3", event_name}]
+    |> registery()
+  end
+
+  defp registery(guards \\ []) do
+    {match_all, map_result} =
+      {
+        {:"$1", :"$2", :"$3"},
+        [%{id: :"$1", pid: :"$2", type: :"$3"}]
+      }
     Registry.select(MishkaInstaller.PluginStateRegistry, [{match_all, guards, map_result}])
   end
 
-
   @spec get_plugin_pid(String.t()) :: {:error, :get_plugin_pid} | {:ok, :get_plugin_pid, pid}
-  def get_plugin_pid(user_id) do
-    case Registry.lookup(MishkaInstaller.PluginStateRegistry, user_id) do
+  def get_plugin_pid(module_name) do
+    case Registry.lookup(MishkaInstaller.PluginStateRegistry, module_name) do
       [] -> {:error, :get_plugin_pid}
       [{pid, _type}] -> {:ok, :get_plugin_pid, pid}
     end

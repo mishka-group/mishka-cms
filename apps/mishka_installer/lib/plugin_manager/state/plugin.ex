b3a9bc1fb79f5f077b5cce21e872ef79219cdd61
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-01-13 00:58:17 +0330

    add schema and add initial files of plugin db and improve Genserver of state

---
+++ apps/mishka_installer/lib/plugin_manager/state/plugin.ex
@@ -0,0 +1,48 @@
+defmodule MishkaInstaller.Plugin do
+
+  alias MishkaDatabase.Schema.MishkaInstaller.Plugin, as: PluginSchema
+  # import Ecto.Query
+  use MishkaDeveloperTools.DB.CRUD,
+          module: PluginSchema,
+          error_atom: :plugin,
+          repo: MishkaDatabase.Repo
+
+
+  @type data_uuid() :: Ecto.UUID.t
+  @type record_input() :: map()
+  @type error_tag() :: :plugin
+  @type repo_data() :: Ecto.Schema.t()
+  @type repo_error() :: Ecto.Changeset.t()
+
+  @behaviour MishkaDeveloperTools.DB.CRUD
+
+  @doc delegate_to: {MishkaDeveloperTools.DB.CRUD, :crud_add, 1}
+  def create(attrs) do
+    crud_add(attrs)
+  end
+
+  @doc delegate_to: {MishkaDeveloperTools.DB.CRUD, :crud_add, 1}
+  def create(attrs, allowed_fields) do
+    crud_add(attrs, allowed_fields)
+  end
+
+  @doc delegate_to: {MishkaDeveloperTools.DB.CRUD, :crud_edit, 1}
+  def edit(attrs) do
+    crud_edit(attrs)
+  end
+
+  @doc delegate_to: {MishkaDeveloperTools.DB.CRUD, :crud_edit, 1}
+  def edit(attrs, allowed_fields) do
+    crud_edit(attrs, allowed_fields)
+  end
+
+  @doc delegate_to: {MishkaDeveloperTools.DB.CRUD, :crud_delete, 1}
+  def delete(id) do
+    crud_delete(id)
+  end
+
+  @doc delegate_to: {MishkaDeveloperTools.DB.CRUD, :crud_get_record, 1}
+  def show_by_id(id) do
+    crud_get_record(id)
+  end
+end
--- apps/mishka_installer/mix.exs
+++ apps/mishka_installer/mix.exs
@@ -9,7 +9,7 @@ defmodule MishkaInstaller.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -18,7 +18,7 @@ defmodule MishkaInstaller.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :mishka_database],
       mod: {MishkaInstaller.Application, []}
     ]
   end
@@ -26,9 +26,8 @@ defmodule MishkaInstaller.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      # {:sibling_app_in_umbrella, in_umbrella: true}
+      {:mishka_database, in_umbrella: true},
+      {:mishka_developer_tools, "~> 0.0.2"}
     ]
   end
 end

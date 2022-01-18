3119e559a0ee616bf771d4cea7912d3f7eded5e2
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-01-18 11:24:30 +0330

    test with new line of information of psql

--- config/test.exs
+++ config/test.exs
@@ -11,13 +11,12 @@ config :mishka_api, MishkaApiWeb.Endpoint,
   server: false
 
 if System.get_env("GITHUB_ACTIONS") do
-  IO.inspect("GITHUB_ACTIONS is true")
-  IO.inspect(System.get_env("DATABASE_URL"))
   config :mishka_database, MishkaDatabase.Repo,
     priv: "apps/mishka_database/priv/repo",
     url: System.get_env("DATABASE_URL") || "postgres://localhost:5432/mishka_test",
     pool: Ecto.Adapters.SQL.Sandbox,
-    pool_size: 10
+    pool_size: 10,
+    show_sensitive_data_on_connection_error: true
 
 else
   config :mishka_database, MishkaDatabase.Repo,

3e6ae2a43ce929556cc2183defc3647e7da869f2
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-03-02 15:09:47 +0330

    edite call back of OnUserAfterLogin event, add using for Hook module

--- apps/mishka_installer/lib/plugin_manager/example/mishka_sending_email_plugin/sending_email.ex
+++ apps/mishka_installer/lib/plugin_manager/example/mishka_sending_email_plugin/sending_email.ex
@@ -1,10 +1,10 @@
 defmodule MsihkaSendingEmailPlugin.SendingEmail do
-  alias MishkaInstaller.{PluginState, Hook}
-  use GenServer, restart: :transient
-  require Logger
-  @ref :on_user_after_login
 
-  @behaviour MishkaInstaller.Reference.OnUserAfterLogin
+  use MishkaInstaller.Hook,
+      module: __MODULE__,
+      behaviour: MishkaInstaller.Reference.OnUserAfterLogin,
+      event: :on_user_after_login,
+      initial: []
 
   def initial(args) do
     Logger.info("SendingEmail plugin was started")
@@ -13,39 +13,25 @@ defmodule MsihkaSendingEmailPlugin.SendingEmail do
     {:ok, @ref, args}
   end
 
-  def call(_user_info, _ip, _endpoint, _status) do
+  def call(%MishkaInstaller.Reference.OnUserAfterLogin{} = _data) do
+    # TODO: this Call function should be used with hook
+    # TODO: should we have a halt status to terminate all the programes?
+    # TODO: send an email without changing state
+    # TODO: store a log for activities
     # {:reply, new_state} | {:noreply, :halt}
   end
 
-  def stop(registerd_info) do
+  def stop(%PluginState{} = registerd_info) do
     case Hook.stop(module: registerd_info.name) do
       {:ok, :stop, msg} -> {:ok, @ref, msg}
       {:error, :stop, msg} -> {:error, @ref, msg}
     end
   end
 
-  def restart(registerd_info) do
+  def restart(%PluginState{} = registerd_info) do
     case Hook.restart(module: registerd_info.name) do
       {:ok, :restart, msg} -> {:ok, @ref, msg}
       {:error, :restart, msg} -> {:error, @ref, msg}
-    end
-  end
-
-  def start_link(_args) do
-    GenServer.start_link(__MODULE__, %{id: "#{__MODULE__}"}, name: __MODULE__)
-  end
-
-  def init(state) do
-    {:ok, state, 300}
-  end
-
-  def handle_info(:timeout, state) do
-    # We should w8 for completing PubSub
-    if is_nil(Process.whereis(MishkaHtml.PubSub)) do
-      {:noreply, state, 100}
-    else
-      initial([])
-      {:noreply, state}
     end
   end
 end

99760ccaf29492734adde72289b7be9bc468f4a9
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-03-05 12:59:04 +0330

    add halt status for testing hook call and add some test module for testing hook

--- apps/mishka_installer/lib/mishka_installer/application.ex
+++ apps/mishka_installer/lib/mishka_installer/application.ex
@@ -4,23 +4,29 @@ defmodule MishkaInstaller.Application do
   @moduledoc false
 
   use Application
-
+  alias MsihkaSendingEmailPlugin.{SendingEmail, SendingSMS, SendingHalt}
   @impl true
   def start(_type, _args) do
     plugin_runner_config = [
       strategy: :one_for_one,
       name: PluginStateOtpRunner
     ]
+
+    test_plugin = if Mix.env in [:test, :dev] do
+      [
+        %{id: SendingEmail, start: {SendingEmail, :start_link, [[]]}},
+        %{id: SendingSMS, start: {SendingSMS, :start_link, [[]]}},
+        %{id: SendingHalt, start: {SendingHalt, :start_link, [[]]}}
+      ]
+    else
+      []
+    end
+
     children = [
       {Registry, keys: :unique, name: PluginStateRegistry},
-      {DynamicSupervisor, plugin_runner_config},
-      %{
-        id: MsihkaSendingEmailPlugin.SendingEmail,
-        start: {MsihkaSendingEmailPlugin.SendingEmail, :start_link, [[]]}
-      }
-    ]
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
+      {DynamicSupervisor, plugin_runner_config}
+    ] ++ test_plugin
+
     opts = [strategy: :one_for_one, name: MishkaInstaller.Supervisor]
     Supervisor.start_link(children, opts)
   end

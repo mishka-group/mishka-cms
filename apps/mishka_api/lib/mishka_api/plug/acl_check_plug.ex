0ffa43f22e1fce50c01d25571b102948a1091066
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-03-08 14:21:30 +0330

    fixe some issue of on_user_authorisation_failure, add on_user_authorisation on api plug

--- apps/mishka_api/lib/mishka_api/plug/acl_check_plug.ex
+++ apps/mishka_api/lib/mishka_api/plug/acl_check_plug.ex
@@ -6,6 +6,7 @@ defmodule MishkaApi.Plug.AclCheckPlug do
   def init(default), do: default
 
   def call(conn, _default) do
+    user_ip = to_string(:inet_parse.ntoa(conn.remote_ip))
     module = case Enum.join(conn.path_info, "/") do
       "" -> "NotFound"
       module -> module
@@ -19,26 +20,38 @@ defmodule MishkaApi.Plug.AclCheckPlug do
          {:user_id_check, false, user_id} <- {:user_id_check, is_nil(get_user_id), get_user_id},
          {:permittes?, true} <- {:permittes?, MishkaUser.Acl.Access.permittes?(action, user_id)} do
 
-          conn
+          on_user_authorisation(conn, user_id, user_ip).conn
     else
-      {:acl_check, true, nil} -> conn
+      {:acl_check, true, nil} -> on_user_authorisation(conn, get_user_id, user_ip).conn
 
       {:user_id_check, true, nil} ->
-        error_message(conn, 401, MishkaTranslator.Gettext.dgettext("api_auth", "شما به این صفحه دسترسی ندارید."))
+        on_user_authorisation_failure(conn, user_ip, {:user_id_check, true, nil}).conn
+        |> error_message(401, MishkaTranslator.Gettext.dgettext("api_auth", "شما به این صفحه دسترسی ندارید."))
 
       {:permittes?, false} ->
-        error_message(conn, 401, MishkaTranslator.Gettext.dgettext("api_auth", "شما به این صفحه دسترسی ندارید."))
+        on_user_authorisation_failure(conn, user_ip, {:permittes?, false}).conn
+        |> error_message(401, MishkaTranslator.Gettext.dgettext("api_auth", "شما به این صفحه دسترسی ندارید."))
     end
   end
 
   defp error_message(conn, status, msg) do
     conn
     |> put_status(status)
-    |> json(%{
-      action: :permission,
-      system: :user,
-      message: msg
-    })
+    |> json(%{action: :permission, system: :user, message: msg})
     |> halt()
+  end
+
+  defp on_user_authorisation_failure(conn, user_ip, error, extra \\ []) do
+    state = %MishkaInstaller.Reference.OnUserAuthorisationFailure{
+      conn: conn, ip: user_ip, endpoint: :api, error: error, module: __MODULE__, operation: :call, extra: extra
+    }
+    MishkaInstaller.Hook.call(event: "on_user_authorisation_failure", state: state)
+  end
+
+  defp on_user_authorisation(conn, user_id, user_ip, extra \\ []) do
+    state = %MishkaInstaller.Reference.OnUserAuthorisation{
+      conn: conn, user_id: user_id, ip: user_ip, endpoint: :api, module: __MODULE__, operation: :call, extra: extra
+    }
+    MishkaInstaller.Hook.call(event: "on_user_authorisation", state: state)
   end
 end
--- apps/mishka_installer/lib/plugin_manager/event/reference/on_user_authorisation_failure.ex
+++ apps/mishka_installer/lib/plugin_manager/event/reference/on_user_authorisation_failure.ex
@@ -1,21 +1,22 @@
 defmodule MishkaInstaller.Reference.OnUserAuthorisationFailure do
   @moduledoc """
     This event is triggered whenever a user gets an error for authorisation. if there is any active module in this section on state,
-    this module sends a request as a Task tool to the developer call function that includes `entries()`, `ip()`, `endpoint()`.
+    this module sends a request as a Task tool to the developer call function that includes `extra()`, `ip()`, `endpoint()`.
     It should be noted; This process does not interfere with the main operation of the system.
     It is just a sender and is active for both side endpoints.
   """
-  defstruct [:ip, :endpoint, :error, :module, :operation, :entries]
+  defstruct [:conn, :ip, :endpoint, :error, :module, :operation, :extra]
 
-  @type entries() :: map() | struct() | tuple()
+  @type extra() :: map() | struct() | list()
   @type error() :: map() | struct() | tuple()
+  @type conn() :: Plug.Conn.t()
   @type ip() :: String.t() # User's IP from both side endpoints connections
   @type endpoint() :: atom() # API, HTML
   @type operation() :: atom()
   @type ref() :: :on_user_authorisation_failure # Name of this event
   @type reason() :: map() | String.t() # output of state for this event
   @type registerd_info() :: MishkaInstaller.PluginState.t() # information about this plugin on state which was saved
-  @type state() :: %__MODULE__{ip: ip(), endpoint: endpoint(), error: error(), module: module(), operation: operation(), entries: entries()}
+  @type state() :: %__MODULE__{conn: conn(), ip: ip(), endpoint: endpoint(), error: error(), module: module(), operation: operation(), extra: extra()}
   @type t :: state() # help developers to keep elixir style
   @type optional_callbacks :: {:ok, ref(), registerd_info()} | {:error, ref(), reason()}
 

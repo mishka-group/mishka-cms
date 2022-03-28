86d80187c33449524cb3c31ca3c044d05c4bf671
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2022-03-28 16:06:42 +0430

    Add facebook: 3, twitter: 4, apple: 5 for EctoEnum UserIdentityProviderEnum (#178)

--- apps/mishka_database/lib/schema/mishka_user/user_schema_enum.ex
+++ apps/mishka_database/lib/schema/mishka_user/user_schema_enum.ex
@@ -1,3 +1,3 @@
 import EctoEnum
-defenum UserIdentityProviderEnum, self: 0, google: 1, github: 2
+defenum UserIdentityProviderEnum, self: 0, google: 1, github: 2, facebook: 3, twitter: 4, apple: 5
 defenum UserStatusEnum, registered: 0, active: 1, inactive: 2, archived: 3

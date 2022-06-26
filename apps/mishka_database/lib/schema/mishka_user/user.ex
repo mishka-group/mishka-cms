93db32addcb7777e24a82a18f1e6bdadf4ab0595
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-06-26 19:39:34 +0430

    add schema and migration of UserToken

--- apps/mishka_database/lib/schema/mishka_user/user.ex
+++ apps/mishka_database/lib/schema/mishka_user/user.ex
@@ -24,6 +24,7 @@ defmodule MishkaDatabase.Schema.MishkaUser.User do
     has_many :subscriptions, MishkaDatabase.Schema.MishkaContent.Subscription, foreign_key: :user_id
     has_many :notifs, MishkaDatabase.Schema.MishkaContent.Notif, foreign_key: :user_id
     has_many :bookmarks, MishkaDatabase.Schema.MishkaContent.Bookmark, foreign_key: :user_id
+    has_many :user_tokens, MishkaDatabase.Schema.MishkaUser.UserToken, foreign_key: :user_id
 
     many_to_many :roles, MishkaDatabase.Schema.MishkaUser.Role, join_through: MishkaDatabase.Schema.MishkaUser.UserRole
 

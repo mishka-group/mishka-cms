824036f1cb0cff59796c3d753d6412a13829cd0f
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-06-28 19:40:24 +0430

    fix #201, add worker to delete random code

--- apps/mishka_user/mix.exs
+++ apps/mishka_user/mix.exs
@@ -19,7 +19,7 @@ defmodule MishkaUser.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :phoenix, :jose],
+      extra_applications: [:logger, :phoenix, :jose, :mishka_database],
       mod: {MishkaUser.Application, []}
     ]
   end
